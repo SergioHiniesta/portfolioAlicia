63da3c72305c3b300f3e1d17770054fe729ebc14
SergioHiniesta <sergio2bp@gmail.com>
2025-02-16 21:42:29 +0100

    create and clean

---
+++ src/components/data/portfolioData.js
@@ -0,0 +1,41 @@
+/**
+ * @SofiDev Esto es JSDOC, si consideras que puede ser complicado solo borralo, es un comentario, no afectará en nada
+ * @typedef PortafolioData
+ * @property {string} imgSrc Url de la imagen
+ * @property {string} title Titulo de la tarjeta
+ * @property {string[]} skills Array con tus habilidades ej: ['React', 'CSS', 'JavaScript']
+ * @property {string} descripcion La descripcion de la tarjeta
+ * @property {string} demoURL Url de una pagina de demostración
+ * @property {string} repoURL Url del repositorio, ej: https://github.com/usuario/repo
+ * @property {string} anim La animación que se ejecutará cuando se cargue la tarjeta, ej: fade-up, fade-right, fade-left, fade-down
+ * @property {number} averageBrightness Cuanto brillo tendrá el color de fondo de la tarjeta, ej: 0.1
+ */
+
+/**
+ * @SofiDev Esto es JSDOC, si consideras que puede ser complicado solo borralo, es un comentario, no afectará en nada
+ * @type {PortafolioData[]}
+ */
+export const portafolioData = [
+	{
+		imgSrc: 'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2021/06/copy-of-the-6-best-restaurant-website-templates-1.png',
+		title: 'Restaurant',
+		skills: ['React', 'StyledComponents'],
+		descripcion:
+			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
+		demoURL: '',
+		repoURL: '',
+		anim: 'fade-right',
+	},
+];
+
+
+/**
+ * @description Se mapea el portafolioData para que tenga los iconos de las habilidades
+ * 	Puedes ver Array.map en https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+ */
+export const getPortafolioData = portafolioData.map((item) => {
+	return {
+		// Se coloca todo el contenido previo del item
+		...item,
+	};
+});

67039cdd1794bad61a3273c79ef378207e79d0dc
SergioHiniesta <sergio2bp@gmail.com>
2025-02-18 19:06:31 +0100

    prueba

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -4,7 +4,7 @@ export const mainMenu = [
      {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"}
 ]
 export const menuBlog = [
-     {link:"/", content:"Home", icon:"lucide:home"},
+     {link:"", content:"Home", icon:"lucide:home"},
      {link:"/#about", content:"About", icon:"octicon:person-24"},
      {link:"/#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
 ]
